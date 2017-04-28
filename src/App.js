e260ef93e6f9ad163874dce524b6fe001ab48c51
Hsyneve <906592157@qq.com>
2017-04-28 15:57:11 +0800

    add task

--- src/App.js
+++ src/App.js
@@ -1,127 +1,169 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import './App.css';
 import 'normalize.css'
 import './reset.css'
 import UserDialog from './UserDialog'
-import { getCurrentUser, signOut, adddata, modifydata, loadfromCloud } from './leanCloud'
+import {
+  getCurrentUser,
+  signOut,
+  adddata,
+  modifydata,
+  loadfromCloud
+} from './leanCloud'
 
 
 class App extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            user: getCurrentUser() || {},
-            newTodo: '',
-            todoList: []
-        }
+    this.state = {
+      user: getCurrentUser() || {},
+      newTodo: '',
+      todoList: []
     }
-    render() {
+  }
+  render() {
 
-        let todos = this.state.todoList.filter((item) => !item.deleted).map((item, index) => {
-            return ( < li key = { index } >
-                < TodoItem todo = { item }
-                onToggle = { this.toggle.bind(this) }
-                onDelete = { this.delete.bind(this) }
-                /> </li >
-            )
-        })
-
-        return ( < div className = "App" >
-            < h1 > { this.state.user.username || '我' }
-            的手账 {
-                this.state.user.id ? < button onClick = { this.signOut.bind(this) } > 登出 < /button> : null}</h
-                1 >
-                    < div className = "inputWrapper" >
-                    < TodoInput content = { this.state.newTodo }
-                onChange = { this.changeTitle.bind(this) }
-                onSubmit = { this.addTodo.bind(this) }
-                /> </div > < ol className = "todoList" > { todos } < /ol> {
-                this.state.user.id ? null : < UserDialog onSignUp = { this.onSignUpOrSignIn.bind(this) }
-                onSignIn = { this.onSignUpOrSignIn.bind(this) }
-                />} </div >
-            )
+    let todos = this.state.todoList.filter((item) => !item.deleted).map((item, index) => {
+      return ( < li key = {
+          index
+        } >
+        <
+        TodoItem todo = {
+          item
         }
-
-        signOut() {
-            signOut()
-            let stateCopy = CopyObj(this.state)
-            stateCopy.user = {}
-            stateCopy.todoList = []
-            this.setState(stateCopy)
+        onToggle = {
+          this.toggle.bind(this)
         }
-
-        onSignUpOrSignIn(user) {
-            let stateCopy = CopyObj(this.state)
-            stateCopy.user = user
-            this.setState(stateCopy)
+        onDelete = {
+          this.delete.bind(this)
         }
-        componentWillMount() {
+        /> </li >
+      )
+    })
 
-
-            let setList = (con) => {
-                let stateCopy = CopyObj(this.state)
-                stateCopy.todoList = con
-                this.setState(stateCopy)
-            }
-
-            loadfromCloud(setList);
-            //
+    return ( < div className = "App" >
+      <
+      h1 > {
+        this.state.user.username || '我'
+      }
+      的手账 {
+        this.state.user.id ? < button onClick = {
+            this.signOut.bind(this)
+          } > 登出 < /button> : null} < /h1> <
+          div className = "inputWrapper" >
+          <
+          TodoInput content = {
+            this.state.newTodo
+          }
+        onChange = {
+          this.changeTitle.bind(this)
         }
-        componentDidUpdate() {
-            if (!this.state.todoList.id) {
-                let setOwner = (id) => { this.state.todoList.id = id }
-                adddata(this.state.todoList, setOwner)
-            } else {
-                modifydata(this.state.todoList)
-            }
+        onSubmit = {
+          this.addTodo.bind(this)
         }
-        changeTitle(event) {
-            this.setState({
-                newTodo: event.target.value,
-                todoList: this.state.todoList
-            })
-
+        /> </div > < ol className = "todoList" > {
+          todos
+        } < /ol> {
+        this.state.user.id ? null : < UserDialog onSignUp = {
+          this.onSignUpOrSignIn.bind(this)
         }
-        toggle(e, todo) {
-            todo.status = todo.status === 'completed' ? '' : 'completed'
-            this.setState(this.state)
-
+        onSignIn = {
+          this.onSignUpOrSignIn.bind(this)
         }
-        addTodo(event) {
-            this.state.todoList.push({
-                id: idMaker(),
-                title: event.target.value,
-                status: '',
-                deleted: false
-            })
-
-            this.setState({
-                newTodo: '',
-                todoList: this.state.todoList
-            })
-
-        }
-        delete(event, todo) {
-            todo.deleted = true
-            this.setState(this.state)
-
-        }
-
-
-
+        />} </div >
+      )
     }
 
-    export default App;
-    let id = 0
-
-    function idMaker() {
-        id = 1
-        return id
+    signOut() {
+      signOut()
+      let stateCopy = CopyObj(this.state)
+      stateCopy.user = {}
+      stateCopy.todoList = []
+      this.setState(stateCopy)
     }
 
-    function CopyObj(obj) {
-        return JSON.parse(JSON.stringify(obj))
+    onSignUpOrSignIn(user) {
+      let stateCopy = CopyObj(this.state)
+      stateCopy.user = user
+      this.setState(stateCopy)
+      let setList = (con) => {
+        let stateCopy = CopyObj(this.state)
+        stateCopy.todoList = con
+        this.setState(stateCopy)
+      }
+      loadfromCloud(setList);
     }
+    componentWillMount() {
+
+
+      let setList = (con) => {
+        let stateCopy = CopyObj(this.state)
+        stateCopy.todoList = con
+        this.setState(stateCopy)
+      }
+
+      loadfromCloud(setList);
+      //
+    }
+    componentDidUpdate() {
+      if (!this.state.todoList.id) {
+        let setOwner = (id) => {
+          this.state.todoList.id = id
+        }
+        adddata(this.state.todoList, setOwner)
+      } else {
+        modifydata(this.state.todoList)
+      }
+    }
+    changeTitle(event) {
+      this.setState({
+        newTodo: event.target.value,
+        todoList: this.state.todoList
+      })
+
+    }
+    toggle(e, todo) {
+      todo.status = todo.status === 'completed' ? '' : 'completed'
+      this.setState(this.state)
+
+    }
+    addTodo(event) {
+      this.state.todoList.push({
+        id: idMaker(),
+        title: event.target.value,
+        status: '',
+        deleted: false
+      })
+
+      this.setState({
+        newTodo: '',
+        todoList: this.state.todoList
+      })
+
+    }
+    delete(event, todo) {
+      todo.deleted = true
+      this.setState(this.state)
+
+    }
+
+
+
+  }
+
+  export default App;
+  let id = 0
+
+  function idMaker() {
+    id = 1
+    return id
+  }
+
+  function CopyObj(obj) {
+    return JSON.parse(JSON.stringify(obj))
+  }
--- src/leanCloud.js
+++ src/leanCloud.js
@@ -1,91 +1,91 @@
 import AV from 'leancloud-storage'
 
 AV.init({
-    appId: 'gHWqAFVNGLrC2kaNdOtOh6DQ-gzGzoHsz',
-    appKey: 'di3QChx0fSpXtfWb89pJL9Gd'
+	appId: 'gHWqAFVNGLrC2kaNdOtOh6DQ-gzGzoHsz',
+	appKey: 'di3QChx0fSpXtfWb89pJL9Gd'
 })
 export default AV
 
 
 export function signUp(username, password, successFn, errorFn) {
-    // 新建 AVUser 对象实例
-    var user = new AV.User()
-        // 设置用户名
-    user.setUsername(username)
-        // 设置密码
-    user.setPassword(password)
-        // 设置邮箱
-    user.signUp().then(function(loginedUser) {
-        let user = getUserFromAVUser(loginedUser)
-        successFn.call(null, user)
-    }, function(error) {
-        errorFn.call(null, error)
-    })
+	// 新建 AVUser 对象实例
+	var user = new AV.User()
+		// 设置用户名
+	user.setUsername(username)
+		// 设置密码
+	user.setPassword(password)
+		// 设置邮箱
+	user.signUp().then(function(loginedUser) {
+		let user = getUserFromAVUser(loginedUser)
+		successFn.call(null, user)
+	}, function(error) {
+		errorFn.call(null, error)
+	})
 
-    return undefined
+	return undefined
 
 }
-export function adddata(con,setOwner) {
-    let dataString = JSON.stringify(con)
-    let AVTodos = AV.Object.extend('ownertodo');
-    let avTodos = new AVTodos();
-    avTodos.set('content', dataString);
-      var acl = new AV.ACL();
-            acl.setReadAccess(AV.User.current(), true);
-            acl.setWriteAccess(AV.User.current(), true);
-            avTodos.setACL(acl);
-    avTodos.save().then(todo => {
-        setOwner.call(null, todo.id);
-    }, function(error) {
+export function adddata(con, setOwner) {
+	let dataString = JSON.stringify(con)
+	let AVTodos = AV.Object.extend('ownertodo');
+	let avTodos = new AVTodos();
+	avTodos.set('content', dataString);
+	var acl = new AV.ACL();
+	acl.setReadAccess(AV.User.current(), true);
+	acl.setWriteAccess(AV.User.current(), true);
+	avTodos.setACL(acl);
+	avTodos.save().then(todo => {
+		setOwner.call(null, todo.id);
+	}, function(error) {
 
-    });
+	});
 }
 export function loadfromCloud(setList) {
-            if (getCurrentUser()) {
-                var query = new AV.Query('ownertodo');
-                query.find().then(todos => {
-                	let ownerList=JSON.parse(todos[0].attributes.content)
-                	ownerList.id=todos[0].id;
-                    setList.call(null, ownerList)
-                  
-                }, function(error) {
-                    // 异常处理
-                });
-            }
-        }
+	if (getCurrentUser()) {
+		var query = new AV.Query('ownertodo');
+		query.find().then(todos => {
+			let ownerList = JSON.parse(todos[0].attributes.content)
+			ownerList.id = todos[0].id;
+			setList.call(null, ownerList)
+
+		}, function(error) {
+			// 异常处理
+		});
+	}
+}
 export function modifydata(con) {
 
-    var todo = AV.Object.createWithoutData('ownertodo', con.id);
-    var dataString = JSON.stringify(con);
-    todo.set('content', dataString);
-    todo.save();
+	var todo = AV.Object.createWithoutData('ownertodo', con.id);
+	var dataString = JSON.stringify(con);
+	todo.set('content', dataString);
+	todo.save();
 }
-export function signIn(username, password, successFn, errorFn,setList) {
-    AV.User.logIn(username, password).then(function(loginedUser) {
-        let user = getUserFromAVUser(loginedUser)
-         loadfromCloud(setList)
-        successFn.call(null, user)
+export function signIn(username, password, successFn, errorFn) {
+	AV.User.logIn(username, password).then(function(loginedUser) {
+		let user = getUserFromAVUser(loginedUser)
 
-    }, function(error) {
-        errorFn.call(null, error)
-    })
+		successFn.call(null, user)
+
+	}, function(error) {
+		errorFn.call(null, error)
+	})
 }
 export function getCurrentUser() {
-    let user = AV.User.current()
-    if (user) {
-        return getUserFromAVUser(user)
-    } else {
-        return null
-    }
+	let user = AV.User.current()
+	if (user) {
+		return getUserFromAVUser(user)
+	} else {
+		return null
+	}
 }
 export function signOut() {
-    AV.User.logOut()
-    return undefined
+	AV.User.logOut()
+	return undefined
 }
 
 function getUserFromAVUser(AVUser) {
-    return {
-        id: AVUser.id,
-        username: AVUser.attributes.username
-    }
+	return {
+		id: AVUser.id,
+		username: AVUser.attributes.username
+	}
 }
